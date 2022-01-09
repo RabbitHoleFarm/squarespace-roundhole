8667f15410730e8bc143f5420820f3f23c028fa8
Jonathan Lomas <jonathan@floatinglomas.ca>
2022-01-09 15:33:01 -0800

    Rewrite so it actually works...

--- code-injection/header.js
+++ code-injection/header.js
@@ -1,32 +1,43 @@
-function fixDates() {
-    var DateTime = luxon.DateTime;
+let DateTime;
 
-    function fixDate(pubDate) {
-        let dateValue = pubDate.getAttribute("datetime") || pubDate.innerText;
-
-        if (pubDate.classList.contains('blog-meta-item--date')) {
-            dateValue = document.querySelector('[itemprop="datePublished"]').getAttribute('content').split("T")[0];
-        }
-
-        pubDate.innerHTML = DateTime
-            .fromISO(dateValue)
-            .toLocaleString(DateTime.DATE_FULL);
+function fixDate(pubDate) {
+    let dateValue = pubDate.getAttribute("datetime") || pubDate.innerText;
+    if (pubDate.classList.contains('blog-meta-item--date')) {
+        dateValue = document
+            .querySelector('[itemprop="datePublished"]')
+            .getAttribute('content')
+            .split("T")[0];
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const pubdates = document.querySelectorAll("time[datetime]:not([class*=event-time]), time[pubdate], time.blog-meta-item--date");
+    pubDate.innerHTML = DateTime
+        .fromISO(dateValue)
+        .toLocaleString(DateTime.DATE_FULL);
+}
 
-        pubdates.forEach(fixDate);
+function fixDates() {
+    DateTime = window.luxon.DateTime;
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document
+            .querySelectorAll("time[datetime]:not([class*=event-time]), time[pubdate], time.blog-meta-item--date")
+            .forEach(fixDate);
     });
 }
 
 (function () {
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js';
+    var script = document.createElement("script");
+    script.src = 'https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js';
 
-    s.onreadystatechange = fixDates;
-    s.onload = fixDates;
+    // IE < 7, does not support onload
+    script.onreadystatechange = function () {
+        if (script.readyState === "loaded" || script.readyState === "complete") {
+            script.onreadystatechange = null;
+            fixDates();
+        }
+    };
 
-    document.head.appendChild(s);
+    // other browsers
+    script.onload = fixDates.bind();
+
+    document.documentElement.firstChild.appendChild(script);
 })();
